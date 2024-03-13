5862e7ebb16e1a96ef943f167d988d1408f447c8
Nimeth <it21095962@my.sliit.lk>
2024-03-13 21:57:36 +0530

    create header component

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,56 @@
 import React from "react";
 
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <h1 className="font-bold">About Ceylon Realty</h1>
+      <p>
+        Welcome to Ceylon Realty, your premier destination for real estate
+        solutions in Sri Lanka.
+      </p>
+
+      <h2>Our Vision</h2>
+      <p>
+        At Ceylon Realty, we envision a future where everyone can find their
+        dream property in the beautiful island nation of Sri Lanka. We strive to
+        be the leading platform connecting buyers, sellers, renters, and
+        investors with the finest properties across the country.
+      </p>
+
+      <h2>Who We Are</h2>
+      <p>
+        Ceylon Realty is more than just a real estate marketplace; we're a team
+        of dedicated professionals passionate about helping you find your
+        perfect home or investment opportunity. With our deep understanding of
+        the local market and commitment to excellence, we're here to guide you
+        through every step of your real estate journey.
+      </p>
+
+      <h2>What We Offer</h2>
+      <p>
+        Whether you're searching for a cozy beachfront villa, a bustling
+        commercial space, or a serene countryside retreat, Ceylon Realty has
+        something for everyone. Our extensive listings feature a diverse range
+        of properties, meticulously curated to meet your unique needs and
+        preferences.
+      </p>
+
+      <h2>Why Choose Us</h2>
+      <p>
+        When you choose Ceylon Realty, you're choosing expertise, integrity, and
+        personalized service. We prioritize transparency and honesty in all our
+        dealings, ensuring that you have the information you need to make
+        informed decisions. With our user-friendly platform and dedicated
+        support team, finding your ideal property has never been easier.
+      </p>
+
+      <h2>Get In Touch</h2>
+      <p>
+        Ready to embark on your real estate journey in Sri Lanka? Explore our
+        listings today or reach out to our friendly team for personalized
+        assistance. At Ceylon Realty, we're here to turn your real estate dreams
+        into reality.
+      </p>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+        "teal-blue": "#007A7C",
+        "golden-yellow": "#FFD700",
+        "earthy-brown": "#8B4513",
+        "terracotta-red": "#E2725B",
+        "creamy-white": "#F5F5F5",
+      },
+    },
   },
   plugins: [],
 };
